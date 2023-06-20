d990d385c2f79fcb41e588f0e1171682fbe8f73a
Sanelisiwe19 <113888456+Sanelisiwe19@users.noreply.github.com>
2023-06-20 23:59:53 +0200

    challenge 2

--- IWA_2/Scripts.js
+++ IWA_2/Scripts.js
@@ -7,6 +7,17 @@ const surname = 'Smith';
 console.log(user, surname);
 
 
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ */
+const date = '10/07/2014';
+
+console.log(date);
+
+
+
 
 
 const MAX_NUMBER = 100
